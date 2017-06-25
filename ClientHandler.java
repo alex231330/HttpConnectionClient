4ecacba40ae9ca2b069179064bc18a45a1b67c99
Alexandr <alexklatch@gmail.com>
2017-06-26 00:44:53 +0300

    Final verison of body

---
+++ ClientHandler.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by alex231330 on 26.06.17.
+ */
+public class ClientHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String req = null;
+        InputStream is = httpExchange.getRequestBody();
+        is.read(req.getBytes());
+    }
+}
---
+++ Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import com.sun.deploy.util.SessionState;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Main {
+
+    static Socket s;
+
+    public static void main(String[] args) throws Throwable {
+        s = new Socket("localhost", 8080);
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        out.println("Test message");
+        StringBuilder sb = new StringBuilder(8096);
+        while (true) {
+            if (input.ready()) {
+                int i = 0;
+                while (i != -1) {
+                    i = input.read();
+                    sb.append((char) i);
+                }
+                break;
+            }
+        }
+        System.out.println(sb.toString());
+        s.close();
+    }
+}
